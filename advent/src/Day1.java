960ea5ed4915eff6b42ff38d5bfdd14c4d0166e8
will <williamboocock@hotmail.com>
2021-12-04 19:08:58 +0000

    Initial commit

---
+++ advent/src/Day1.java
@@ -0,0 +1,8 @@
+public class Day1 {
+
+    /*import input file
+    for each number check whether it is bigger than the number before
+    if it is then increment counter by one
+    output final number
+     */
+}
